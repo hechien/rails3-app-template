c0dd193eacc69d5942d38deb3878d6c4eb906fc8
newlix <newlix.yang@gmail.com>
2012-02-21 20:52:05 +0800

    update to rails3.2 add simpleform 2.0 support with bootstrap

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.3'
+gem 'rails', '3.2.1'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
@@ -11,8 +11,8 @@ gem 'sqlite3'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails'
-  gem 'coffee-rails', '~> 3.1.1'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
 
@@ -32,7 +32,6 @@ gem 'therubyracer'
 
 group :test, :development do
   # Pretty printed test output
-  gem 'turn', '~> 0.8.3', :require => false
   gem "watchr"
   #gem "delorean"
 end
@@ -40,7 +39,7 @@ gem 'devise'
 gem 'anjlab-bootstrap-rails', :require => 'bootstrap-rails'
 gem 'haml-rails'
 gem 'kaminari'
-gem 'simple_form', :git => 'git://github.com/plataformatec/simple_form.git'
+gem 'simple_form'
 
 
 
--- rails3.rb
+++ rails3.rb
@@ -11,27 +11,13 @@ file 'Gemfile', File.read("#{File.dirname(rails_template)}/Gemfile")
 # bundle install
 run "bundle install"
 
-# generate rspec
-generate "rspec:install"
+# install simpleform with bootstrap support
+
+rails generate simple_form:install --bootstrap
 
 # copy files
 file 'script/watchr.rb', File.read("#{File.dirname(rails_template)}/watchr.rb")
 file 'lib/tasks/dev.rake', File.read("#{File.dirname(rails_template)}/dev.rake")
-
-# remove active_resource and test_unit
-gsub_file 'config/application.rb', /require 'rails\/all'/, <<-CODE
-  require 'rails'
-  require 'active_record/railtie'
-  require 'action_controller/railtie'
-  require 'action_mailer/railtie'
-CODE
-
-# install jquery
-run "curl -L http://code.jquery.com/jquery.min.js > public/javascripts/jquery.js"
-run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
-
-gsub_file 'config/application.rb', /(config.action_view.javascript_expansions.*)/, 
-                                   "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)"
 
 # add time format
 environment 'Time::DATE_FORMATS.merge!(:default => "%Y/%m/%d %I:%M %p", :ymd => "%Y/%m/%d")'
